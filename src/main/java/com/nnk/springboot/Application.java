14268cfec3530d0caf2678b23205b3eda10eec4c
laflosh <l_florent_l@hotmail.fr>
2024-12-16 14:58:10 +0100

    Config log for project

--- src/main/java/com/nnk/springboot/Application.java
+++ src/main/java/com/nnk/springboot/Application.java
@@ -1,12 +1,19 @@
 package com.nnk.springboot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+	private static final Logger log = LogManager.getLogger("Application");
 
 	public static void main(String[] args) {
+		
+		log.info("Initalizing API Poseidon Capital Solution");
 		SpringApplication.run(Application.class, args);
+		
 	}
 }
--- src/main/java/com/nnk/springboot/domain/BidList.java
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -6,5 +6,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "bidlist")
 public class BidList {
+	
     // TODO: Map columns in data table BIDLIST with corresponding java fields
+	
 }
