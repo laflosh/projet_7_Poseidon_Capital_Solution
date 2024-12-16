70a33e0a914602db358443d26c0fb3c4551f79b2
laflosh <l_florent_l@hotmail.fr>
2024-12-16 17:16:55 +0100

    Implementation rating domain

--- src/main/java/com/nnk/springboot/domain/Rating.java
+++ src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,78 @@
 package com.nnk.springboot.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-import java.sql.Timestamp;
 
 @Entity
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private  Integer id;
+	
+	@Column(name = "moodysRating")
+	private String moodysRating;
+	
+	@Column(name = "sandRating")
+	private String sandRating;
+	
+	@Column(name = "fitchRating")
+	private String fitchRating;
+	
+	@Column(name = "orderNumber")
+	private Integer orderNumber;
+	
+	public Rating(String moodysRating, String sandRating, String fitchRating, Integer orderNumber){
+		this.moodysRating = moodysRating;
+		this.sandRating = sandRating;
+		this.fitchRating = fitchRating;
+		this.orderNumber = orderNumber;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getMoodysRating() {
+		return moodysRating;
+	}
+
+	public void setMoodysRating(String moodysRating) {
+		this.moodysRating = moodysRating;
+	}
+
+	public String getSandRating() {
+		return sandRating;
+	}
+
+	public void setSandRating(String sandRating) {
+		this.sandRating = sandRating;
+	}
+
+	public String getFitchRating() {
+		return fitchRating;
+	}
+
+	public void setFitchRating(String fitchRating) {
+		this.fitchRating = fitchRating;
+	}
+
+	public Integer getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(Integer orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	
 }
