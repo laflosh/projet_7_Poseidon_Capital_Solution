f95520269a9ccd76a7676f8244ac0c938dc9b183
laflosh <l_florent_l@hotmail.fr>
2024-12-16 16:58:09 +0100

    Implementation curvepoint domain

--- src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,10 +1,90 @@
 package com.nnk.springboot.domain;
 
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "curveId")
+	private Integer curveId;
+	
+	@Column(name = "asOfDate")
+	private Timestamp asOfDate;
+	
+	@Column(name = "term")
+	private double term;
+	
+	@Column(name = "value")
+	private double value;
+	
+	@Column(name = "creationDate")
+	private Timestamp creationDate;
+	
+	public CurvePoint(Integer curveId, double term, double value) {
+		this.curveId = curveId;
+		this.term = term;
+		this.value = value;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCurveId() {
+		return curveId;
+	}
+
+	public void setCurveId(Integer curveId) {
+		this.curveId = curveId;
+	}
+
+	public Timestamp getAsOfDate() {
+		return asOfDate;
+	}
+
+	public void setAsOfDate(Timestamp asOfDate) {
+		this.asOfDate = asOfDate;
+	}
+
+	public double getTerm() {
+		return term;
+	}
+
+	public void setTerm(double term) {
+		this.term = term;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public Timestamp getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Timestamp creationDate) {
+		this.creationDate = creationDate;
+	}
+	
 }
