836b58f46970c0e53f59c291fe61ca50bc8860b3
laflosh <l_florent_l@hotmail.fr>
2024-12-22 16:52:20 +0100

    Implementation rulename domain

--- src/main/java/com/nnk/springboot/domain/RuleName.java
+++ src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -1,12 +1,82 @@
 package com.nnk.springboot.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-import java.sql.Timestamp;
 
 @Entity
 @Table(name = "rulename")
 public class RuleName {
-    // TODO: Map columns in data table RULENAME with corresponding java fields
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	Integer id;
+	
+	@Column(name = "name")
+	String name;
+	
+	@Column(name = "json")
+	String json;
+	
+	@Column(name = "template")
+	String template;
+	
+	@Column(name = "sqlStr")
+	String sqlStr;
+	
+	@Column(name = "sqlPart")
+	String sqlPart;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJson() {
+		return json;
+	}
+
+	public void setJson(String json) {
+		this.json = json;
+	}
+
+	public String getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+
+	public String getSqlStr() {
+		return sqlStr;
+	}
+
+	public void setSqlStr(String sqlStr) {
+		this.sqlStr = sqlStr;
+	}
+
+	public String getSqlPart() {
+		return sqlPart;
+	}
+
+	public void setSqlPart(String sqlPart) {
+		this.sqlPart = sqlPart;
+	}
+	
 }
