8934ecb06dd18c7655fef05ce61ab5b7a4979a68
laflosh <l_florent_l@hotmail.fr>
2025-01-14 15:48:50 +0100

    get all bidlists in database

--- src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -1,9 +1,16 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.service.BidListService;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,16 +19,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class BidListController {
-    // TODO: Inject Bid service
+	
+	private static  final Logger log = LogManager.getLogger(BidListController.class);
+    
+	@Autowired
+	BidListService bidListService;
 
     @RequestMapping("/bidList/list")
-    public String home(Model model)
+    public String home(Model model, Authentication auth)
     {
-        // TODO: call service find all bids to show to the view
+    	log.info("Trying to get all bidlists in database");
+
+    	List<BidList> bidLists = bidListService.getAllBidLists();
+    	
+    	model.addAttribute("bidLists", bidLists); 
+    	model.addAttribute("username", auth.getName());    	
         return "bidList/list";
+        
     }
 
     @GetMapping("/bidList/add")
@@ -42,8 +58,7 @@ public class BidListController {
     }
 
     @PostMapping("/bidList/update/{id}")
-    public String updateBid(@PathVariable("id") Integer id, @Valid BidList bidList,
-                             BindingResult result, Model model) {
+    public String updateBid(@PathVariable("id") Integer id, @Valid BidList bidList, BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Bid and return list Bid
         return "redirect:/bidList/list";
     }
---
+++ src/main/java/com/nnk/springboot/service/BidListService.java
@@ -0,0 +1,28 @@
+package com.nnk.springboot.service;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.repositories.BidListRepository;
+
+@Service
+public class BidListService {
+	
+	private static  final Logger log = LogManager.getLogger(BidListService.class);
+	
+	@Autowired
+	BidListRepository bidListRepository;
+
+	public List<BidList> getAllBidLists() {
+		
+		List<BidList> bidLists = bidListRepository.findAll();
+		
+		return bidLists;
+	}
+
+}
